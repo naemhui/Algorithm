6c1171d1c77c61784c63150abd6417317358cc29
naemhui <knaem.edu@gmail.com>
2024-08-11 06:53:09 +0900

    0811

--- 백준/test.py
+++ 백준/test.py
@@ -55,5 +55,65 @@
 # for i in range(0, 7, 4):
 #     print(i)
 
-light = list(input())
-print(light)
+# light = list(input())
+# print(light)
+
+# arr = [[1, 2, 3], [4, 5, 6]]
+# # print(arr[0])
+# a = arr[0]
+# if a.index(2):
+#     print('good')
+
+arr1 = [list(map(int, input().split())) for _ in range(5)]
+arr2 = [list(map(int, input().split())) for _ in range(5)]
+bingo = [[0]*5 for _ in range(5)]
+
+clear = 0
+def bingo_check(bingo, i, j):
+
+    # 가로 빙고 확인
+    for r in bingo:
+        if sum(r) == 5:
+            clear += 1
+
+    # 세로 빙고 확인
+    for c in range(5):
+        sum_c = 0
+        for r in range(5):
+            sum_c += bingo[r][c]
+        if sum_c == 5:
+            clear += 1
+
+    # 대각선 빙고 확인
+    sum_d1 = 0
+    for r in range(5):
+        sum_d1 += bingo[r][r]
+    if sum_d1 == 5:
+        clear += 1
+    
+    # 대각선 빙고 확인2
+    sum_d2 = 0
+    for r in range(5):
+        sum_d2 += bingo[r][4-r]
+    if sum_d2 == 5:
+        clear += 1
+
+    return clear
+
+
+cnt = 0
+
+for i in range(5):
+    for j in range(5):
+        num = arr2[i][j]  # 사회자가 부른 num
+        cnt += 1
+
+        for k in range(5):    
+            if num in arr1[k]:  # num 인덱스 저장
+                idx = arr1[k].index(num)
+                bingo[k][idx] = 1
+
+        clear = bingo_check(bingo, i, j)
+        if clear == 3:
+            print(cnt)
+            
