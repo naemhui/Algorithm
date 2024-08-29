88b1a6fece312af674516b69fa4b51f6163448db
권남희 <knaem.edu@gmail.com>
2024-08-29 16:24:01 +0900

    0829 퇴사: 동적계획법 claude

--- 백준/14501_quit.py
+++ 백준/14501_quit.py
@@ -3,10 +3,10 @@ def max_profit(N, T, P):
     dp = [0] * (N + 1)
     
     # 마지막 날부터 첫째 날까지 역순으로 계산
-    for i in range(N-1, -1, -1):
+    for i in range(N - 1, -1, -1):
         if i + T[i] > N:
             # 상담이 퇴사일을 넘어가는 경우
-            dp[i] = dp[i+1]
+            dp[i] = dp[i + 1]
         else:
             # 현재 상담을 하는 경우와 하지 않는 경우 중 최대값 선택
             dp[i] = max(P[i] + dp[i + T[i]], dp[i + 1])
