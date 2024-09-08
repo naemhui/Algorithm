eff1599ef4955ad91fe9068e586a549e973adb56
naemhui <knaem.edu@gmail.com>
2024-09-08 16:47:24 +0900

    0908 가장 긴 감소하는 부분 수열

--- 백준/11722.py
+++ 백준/11722.py
@@ -1,25 +1,11 @@
-'''
-현재 수열 길이 = 이전 수열 길이 + 1
-'''
-
-def cnt_seq(idx):
-    global cnt
-    global max_cnt
-
-    if idx+1 >= A:
-        return max_cnt
-    
-    if sequence[idx+1] < sequence[idx]:
-        cnt += 1
-        cnt_seq(idx+1)
-    else:
-        max_cnt = max(max_cnt, cnt)
-        cnt = 0
-        cnt_seq(idx+1)
-
-
 A = int(input())
 sequence = list(map(int, input().split()))
-cnt = 0
-max_cnt = 0
-print(cnt_seq(0))
+dp = [1]*A
+# dp[i]에 i까지 봤을 때 가장 긴 감소하는 부분수열의 길이 저장
+
+for i in range(1, A):
+    for j in range(i):
+        if sequence[i] < sequence[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(max(dp))
