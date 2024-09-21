cf455f47dbc80c20163ae9833812bd27c9d1732d
naemhui <knaem.edu@gmail.com>
2024-09-22 03:12:15 +0900

    0922 문자 해독

--- 백준/1593.py
+++ 백준/1593.py
@@ -11,26 +11,44 @@ g, s = map(int, input().split())
 W = input().strip()
 S = input().strip()
 
+# 알파벳의 개수는 26개 (대소문자 포함시 52개)
+# W와 S의 문자 빈도수를 저장
+w_count = [0] * 52
+s_count = [0] * 52
 
-visited = [False]*g
-results = set()
+# 알파벳 인덱스 계산 (대소문자 구분)
+def char_index(c):
+    if 'A' <= c <= 'Z':
+        return ord(c) - ord('A')
+    else:
+        return ord(c) - ord('a') + 26
 
-def dfs(level, now_alp):
-    if level == g:
-        results.add(now_alp)
-        return
-    
-    for i in range(g):
-        if not visited[i]:
-            visited[i] = True
-            dfs(level+1, now_alp + W[i])
-            visited[i] = False
-dfs(0,'')
+# W의 문자 빈도수 계산
+for c in W:
+    w_count[char_index(c)] += 1
+
+# S의 첫 번째 윈도우(길이 g)의 문자 빈도수 계산
+for i in range(g):
+    s_count[char_index(S[i])] += 1
+
+# 두 배열이 같은지 확인
+def is_equal():
+    for i in range(52):
+        if w_count[i] != s_count[i]:
+            return False
+    return True
 
 cnt = 0
-for i in range(s-g+1):
-    word = S[i:i+g]
-    if word in results:
+if is_equal():
+    cnt += 1
+
+# 슬라이딩 윈도우 - s 탐색
+for i in range(g, s):
+    s_count[char_index(S[i])] += 1  # 새로운 문자 추가
+    s_count[char_index(S[i - g])] -= 1  # 윈도우에서 벗어난 문자 제거
+    
+    if is_equal():  # 두 배열이 같으면
         cnt += 1
 
 print(cnt)
+
