b01aa4b927aa7a2e8758059f05e27b044765d5c3
naemhui <knaem.edu@gmail.com>
2024-10-10 22:09:26 +0900

    1010 코스피_하늘에서 별똥별이 빗발친다 어렵다

--- 백준/14658.py
+++ 백준/14658.py
@@ -2,81 +2,29 @@
 별똥별 수 최소화
 트램펄린 L*L
 '''
-# N: 가로, M: 세로, L:트램펄린 한 변, K:별똥별 수
-N, M, L, K = map(int, input().split())
-arr = [[0]*(N+1) for _ in range(M+1)]
-for _ in range(K):
+n, m, l, k = map(int, input().split())
+
+stars = []
+for _ in range(k):
     x, y = map(int, input().split())
-    arr[y][x] = 1
-# print(arr)
-# 시계방향으로 굴러가게(달팽이st)
-# 우 하 좌 상
-dr = [0, 1, 0, -1]
-dc = [1, 0, -1, 0]
+    stars.append((x, y))
 
-def is_valid(r,c, nr, nc):
-    return r<=nr<L+r and c<=nc<L+c
+bounce = 0  # 튕겨낸 별똥별 최대 수
 
+# 각 별똥별을 기준으로 트램펄린을 배치할 수 있는 모든 가능한 좌표를 확인
+for starA in stars:  # 트램펄린의 가로 위치 결졍 (별똥별 A 기준)
+    for starB in stars:  # 트램펄린의 세로 위치 결정 (별똥별 B 기준)
+        cnt = 0
+        # 트램펄린의 왼쪽 아래 모서리 좌표가 (starA[0], starB[1])인 경우
+        x1, y1 = starA[0], starB[1]
 
-max_v = 0
-for r in range(M+1):
-    for c in range(N+1):
-        # 별 있으면 탐색 (모든 점에서 탐색 X)
-        if arr[r][c] == 1:
-            d = 0
-            cnt = 1
-            sum_v = arr[r][c]
-            # while 1:
-            for i in range(L*L):
-                if cnt == L*L:
-                    max_v = max(max_v, sum_v)
-                    break
-                nr = r + dr[d]
-                nc = c + dc[d]
-                print(d)
-                # cnt += 1
-                if is_valid(r,c,nr,nc) and 0<=nr<=M and 0<=nc<=N: # 다음 위치 유효하면
-                    r, c = nr, nc  # 그대로 진행
-                    cnt += 1
+        # 트램펄린 범위 안에 들어오는 별똥별을 세기
+        for starC in stars:
+            if x1 <= starC[0] <= x1 + l and y1 <= starC[1] <= y1 + l:
+                cnt += 1
 
-                else:
-                    d = d + 1 if d < 3 else 0
-                    r = r + dr[d]
-                    c = c + dc[d]
-                    cnt += 1
-            
-print(max_v)
+        # 최대로 튕겨낸 별똥별 수 갱신
+        bounce = max(bounce, cnt)
 
-# 상하좌우 순서
-# dr = [0, 1, 0, -1]  # 행
-# dc = [1, 0, -1, 0]  # 열
-
-# T = int(input())
-# for tc in range(1, T+1):
-#     N = int(input())
-#     snail = [[0]*N for _ in range(N)]
-
-#     r, c = 0, 0  # 출발 위치
-#     d = 0  # 방향 인덱스
-
-#     for i in range(1, N*N+1):
-#         snail[r][c] = i  # 달팽이 1부터 채워짐
-
-#         # 다음 행렬 인덱스
-#         nr = r + dr[d]  # 다음 행 인덱스
-#         nc = c + dc[d]  # 다음 열 인덱스
-
-#         # 유효 인덱스 검사
-#         if 0 <= nr <N and 0 <= nc < N and snail[nr][nc] == 0: # 다음 위치 유효하면
-#             r, c = nr, nc  # 그대로 진행
-
-#         else:
-#             d = d + 1 if d < 3 else 0  # 유효한 인덱스가 되도록(d는 0, 1, 2, 3)
-#             r = r + dr[d]
-#             c = c + dc[d]
-
-#     print(f'#{tc}')
-
-#     for i in range(N):
-#         print(*snail[i])
-
+# 튕겨내지 못한 별똥별의 수 출력
+print(k - bounce)
