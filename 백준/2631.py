cfec4f31ee9adb64c3c0c56093464fd671db2dbc
권남희 <knaem.edu@gmail.com>
2024-10-23 17:02:05 +0900

    줄 세우기-틀림

--- 백준/2631.py
+++ 백준/2631.py
@@ -1,24 +1,26 @@
 '''
-선택 정렬 문제인가 이거
+주어진 배열에서 최장 증가 부분 수열(LIS)의 길이를 찾고
+이를 통해 필요한 이동 수를 계산하는 방법으로 풀자
+
+-> 원래 배열에서 이미 정렬된 상태에 있는 부분을 찾아 그 길이를 LIS로 사용
+-> 총 아이들의 수 - LIS 길이 = 이동해야 할 최소 수
 '''
 
-def selection_sort(arr, N):
-    cnt = 0
+def min_sort(arr, N):
+    sorted_arr = sorted(arr)
+    lis_length = 0
+    dp = [1] * N
 
-    for i in range(N-2):
-        # min_idx = arr[min(arr)]
-        min_idx = i  # 최솟값 위치를 기준위치로
-        for j in range(i+1, N):
-            if arr[min_idx] > arr[j]:
-                min_idx = j
+    # LIS 길이 계산
+    for i in range(N):
+        for j in range(i):
+            if arr[i] == sorted_arr[j]:  # 정렬된 배열과 일치하면
+                dp[i] = max(dp[i], dp[j] + 1)
 
-        # 구간의 최솟값을 기준위치로 이동
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
-        cnt += 1
+    lis_length = max(dp)
 
-        if arr == sorted(arr):
-            return cnt
+    return N - lis_length
 
 N = int(input())
 arr = list(int(input()) for _ in range(N))
-print(selection_sort(arr, N))
+print(min_sort(arr, N))
