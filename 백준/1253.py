f211f4baf9344759156bceebab8edc6b475e3ecc
권남희 <knaem.edu@gmail.com>
2024-10-23 14:11:52 +0900

    좋다-투 포인터

--- 백준/1253.py
+++ 백준/1253.py
@@ -1,3 +1,13 @@
+'''
+두 포인터 : left, right
+left : 가장 작은 원소 -> 큰 원소
+right : 가장 큰 원소 -> 작은 원소
+
+왜?
+-> 두 수의 합이 target보다 작으면 left를 증가,
+-> target보다 크면 더 작은 수를 사용하기 위해 right를 감소
+'''
+
 N = int(input())
 arr = list(map(int, input().split()))
 arr.sort()
@@ -14,9 +24,12 @@ for i in range(N):
         now_sum = arr[left] + arr[right]
 
         if now_sum == target:
+            # 자기 자신(target) 제외한 두 수인지
             if left != i and right != i:
                 cnt += 1
                 break
+
+            # target이 left 또는 right와 같으면
             else:
                 left += 1
                 right -= 1
