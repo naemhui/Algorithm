37ab93c068ad032b3b02c7bd0ec970f8452b4d24
naemhui <knaem.edu@gmail.com>
2024-10-30 01:56:01 +0900

    알파벳 - dfs 시도

--- 백준/1987.py
+++ 백준/1987.py
@@ -1,18 +1,20 @@
+import sys
+input = sys.stdin.readline
+
 R, C = map(int, input().split())
-board = [list(input()) for _ in range(R)]
+board = [list(input().strip()) for _ in range(R)]
 
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
 
-visited = []
-max_cnt = 0
-r, c = 0, 0  # 시작 위치
+visited = []  # 방문한 알파벳 저장
+max_cnt = 0  # 최대 경로 길이
 
 def is_valid(r, c):
-    return 0<=r<R and 0<=c<C
+    return 0 <= r < R and 0 <= c < C
 
-def dfs(cnt):
-    global max_cnt, r, c
+def dfs(r, c, cnt):
+    global max_cnt
     max_cnt = max(max_cnt, cnt)
 
     for i in range(4):
@@ -20,15 +22,11 @@ def dfs(cnt):
         nc = c + dc[i]
 
         if is_valid(nr, nc) and board[nr][nc] not in visited:
-            visited.append(board[nr][nc])
-            # 현재 위치 업데이트 후 dfs
-            prev_r, prev_c = r, c  # 이전 위치 저장
-            r, c = nr, nc
-            dfs(cnt + 1)
-            r, c = prev_r, prev_c  # 원래 위치로 되돌리기
-            visited.pop()
+            visited.append(board[nr][nc])  # 알파벳 방문 기록
+            dfs(nr, nc, cnt + 1)
+            visited.pop()  # 방문 기록 제거
 
-visited.append(board[0][0])  # 시작 칸 알파벳 추가
-dfs(1)
+visited.append(board[0][0])
+dfs(0, 0, 1)
 
 print(max_cnt)
