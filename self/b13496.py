5ccb0b6aa3a123baab91640ab3cb1a2a5c6482fd
권남희 <knaem.edu@gmail.com>
2024-11-04 13:24:20 +0900

    The Merchant of Venice 시도

---
+++ self/b13496.py
@@ -0,0 +1,12 @@
+K = int(input())
+for _ in range(K):
+    n, s, d = map(int, input().split())
+    ans = 0
+
+    for i in range(n):
+        di, vi = map(int, input().split())
+        if d*s >= di:
+            ans = ans + vi
+    
+    print(f'Data Set {_+1}:')
+    print(ans)
