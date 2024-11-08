e6bd575487758a0edc0003ca8b9a3779512f3585
권남희 <knaem.edu@gmail.com>
2024-11-08 10:58:46 +0900

    특별한 학교 이름

---
+++ self/b27889.py
@@ -0,0 +1,10 @@
+schools = {
+    'NLCS': 'North London Collegiate School',
+    'BHA': 'Branksome Hall Asia',
+    'KIS': 'Korea International School',
+    'SJA': 'St. Johnsbury Academy'
+    }
+
+S = input()
+if S in schools:
+    print(schools[S])
