01e764cc20b2974f9ac071c4ea1bdb40c507ab7b
박찬미 <knaem.edu@gmail.com>
2024-11-15 09:07:27 +0900

    엘리스 트랙 매칭

---
+++ self/b16234.py
@@ -0,0 +1,6 @@
+from collections import deque
+graph = []
+def bfs()
+
+N, L, R = map(int, input().split())
+for _ in range(N):
---
+++ self/b31428.py
@@ -0,0 +1,6 @@
+N = int(input())
+friends = input().split()
+# print(friends)
+# H = input()
+
+print(friends.count(input()))
